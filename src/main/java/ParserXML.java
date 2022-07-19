90744ee4c4449c0749b79432601e0e91e7602211
Egor51 <kubrakov_egor87@mail.ru>
2022-07-19 15:28:22 +0300

    create README

--- src/main/java/ParserXML.java
+++ src/main/java/ParserXML.java
@@ -10,37 +10,6 @@ import java.util.List;
 
 public class ParserXML {
 
-//    public static List<Employee> parserXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
-//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//        DocumentBuilder builder = factory.newDocumentBuilder();
-//        Document doc = builder.parse(new File(fileName));
-//        Node root = doc.getDocumentElement();
-//        System.out.println("Корневой элемент: " + root.getNodeName());
-//
-//        read(root);
-//
-//        return new ArrayList<>();
-//    }
-//
-//    public static void read(Node node) {
-//        NodeList nodeList = node.getChildNodes();
-//        for (int i = 0; i < nodeList.getLength(); i++) {
-//            Node node_ = nodeList.item(i);
-//            if (Node.ELEMENT_NODE == node_.getNodeType()) {
-//                System.out.println("Текущий узел: " + node_.getNodeName());
-//                Element element = (Element) node_;
-//                NamedNodeMap map = element.getAttributes();
-//                for (int a = 0; a < map.getLength(); a++) {
-//                    String attrName = map.item(a).getNodeName();
-//                    String attrValue = map.item(a).getNodeValue();
-//                    System.out.println("Атрибут: " + attrName + "; значение: " + attrValue);
-//                        }
-//                        read(node_);
-//                    }
-//                }
-//            }
-//        }
-
     public static List<Employee> parseXML(String fileName) throws IOException, SAXException, ParserConfigurationException {
         List<Employee> staff = new ArrayList<>();
         try {
